4ac0a45f94c19a7a68d0ea33c004e4d4f1dc4c3b
Tim Sullivan <timothyj.sullivan1@gmail.com>
2017-10-18 23:48:23 -0500

    Updated to use Ubuntu
    I was having issues with Centos sharing the vagrant directory, so I have
    switched all scripts to use Ubuntu commands.

--- init_git_config.sh
+++ init_git_config.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -x
 
+# Login to AWS via SAML
+python3 /opt/tools/aws_saml_auth/aws_saml_auth.py
+
 # Configure git to know what to do with CodeCommit Repos
 git config --global credential.https://git-codecommit.us-east-1.amazonaws.com.helper '!aws codecommit credential-helper $@'
 git config --global credential.https://git-codecommit.us-east-1.amazonaws.com.UseHttpPath true
 
 git config --global credential.https://git-codecommit.us-east-2.amazonaws.com.helper '!aws codecommit credential-helper $@'
 git config --global credential.https://git-codecommit.us-east-2.amazonaws.com.UseHttpPath true
+
+# Clone tooling repository
+mkdir Code
+cd Code/
+git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/ITS-Tooling
--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -x
 
+# Update box
+sudo apt-get update -y
+# Do not upgrade box -- this includes a GRUB update that I haven't worked through sudo apt-get upgrade -y
+
 # Install packages
-sudo yum install git -y
-sudo yum install gcc -y
-sudo yum install epel-release -y
-sudo yum install python34-setuptools -y
-sudo yum install python34-pip -y
+sudo apt-get install python3-pip -y
+sudo apt-get install dos2unix -y
+
 sudo pip3 install --upgrade pip
-sudo yum install dos2unix -y
 
 # Create tooling directory
 sudo mkdir -p /opt/tools
@@ -25,6 +26,9 @@ sudo python3 install_hashi_tools.py
 cd /opt/tools
 sudo git clone https://github.com/tjsullivan1/aws_saml_auth.git
 sudo pip3 install boto3 bs4 awscli requests configparser lxml plac
+
+# Make sure that we can run our initialization script.
+dos2unix /vagrant/init_git_config.sh
 
 ### TODO ###
 # Copy init_git_config.sh
